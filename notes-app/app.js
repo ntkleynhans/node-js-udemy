9fd27e22d2f365d76154ca6d47af1c74233088cf
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-29 15:04:24 +0200

    import own modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,12 @@
-const fs = require('fs');
+/*const fs = require('fs');
 
 fs.writeFileSync('notes.txt', 'This file was created by node.js');
 
 fs.appendFileSync('notes.txt', 'Some text has been appended to the file');
+*/
+
+const add = require('./utils');
+const getNotes = require('./notes');
+
+console.log(add(2,3));
+console.log(getNotes());
