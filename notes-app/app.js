ff76776628dc515b7489876b840502e80cd7dfc8
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-29 15:29:06 +0200

    importing a npm module validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,8 +5,10 @@ fs.writeFileSync('notes.txt', 'This file was created by node.js');
 fs.appendFileSync('notes.txt', 'Some text has been appended to the file');
 */
 
+const validator = require('validator');
 const add = require('./utils');
 const getNotes = require('./notes');
 
 console.log(add(2,3));
 console.log(getNotes());
+console.log(validator.isEmail('neil@example.com'));
