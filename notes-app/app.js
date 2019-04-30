0a92c5279f7511a019a15ac6a3191b88d1a41c1d
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-30 09:10:39 +0200

    using chalk for console logging pretty

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,11 +4,14 @@ fs.writeFileSync('notes.txt', 'This file was created by node.js');
 
 fs.appendFileSync('notes.txt', 'Some text has been appended to the file');
 */
-
+const chalk = require('chalk');
 const validator = require('validator');
 const add = require('./utils');
 const getNotes = require('./notes');
+const log = console.log
 
-console.log(add(2,3));
-console.log(getNotes());
-console.log(validator.isEmail('neil@example.com'));
+log(chalk.green('Success!'));
+log(chalk.green.inverse('Success!'));
+log(chalk.blue.bgRed.bold(add(2,3)));
+log(chalk.blue(getNotes()));
+log(chalk.green.bold(validator.isEmail('neil@example.com')));
