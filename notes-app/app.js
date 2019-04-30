ec02aba35821a5eea17ef6eb543f27f7bfea97ba
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-30 14:15:38 +0200

    using process argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -8,10 +8,23 @@ const chalk = require('chalk');
 const validator = require('validator');
 const add = require('./utils');
 const getNotes = require('./notes');
-const log = console.log
 
-log(chalk.green('Success!'));
+const log = console.log;
+const error = chalk.red;
+const warn = chalk.yellow;
+const info = chalk.green;
+
+/*log(chalk.green('Success!'));
 log(chalk.green.inverse('Inverse!'));
 log(chalk.blue.bgRed.bold(add(2,3)));
 log(chalk.blue(getNotes()));
-log(chalk.green.bold(validator.isEmail('neil@example.com')));
+log(chalk.green.bold(validator.isEmail('neil@example.com')));*/
+
+switch(process.argv[2]) {
+  case 'add':
+    log(info('Adding'));
+    break;
+  default:
+    log(error(`Unknown command: ${process.argv[2]}`));
+    break;
+}
